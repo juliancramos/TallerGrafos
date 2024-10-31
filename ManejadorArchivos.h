76af5a1c5d76548a54149a1c1bfc014e0969bcf0
Julian Ramos <julianc.ramos21@gmail.com>
2024-10-31 00:39:28 -0500

    Metodos de entrada y salida de archivos

--- ManejadorArchivos.h
+++ ManejadorArchivos.h
@@ -4,11 +4,16 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include "Grafo.h"
+#include <sstream>
 
 class ManejadorArchivos {
 public:
-    // Método para leer el archivo y mostrar su contenido
-    void ManejadorArchivos::leerArchivo(const std::string& nombreArchivo, Grafo<std::string>& grafo) const;
+    // Para leer el archivo de conexiones y guardar en el grafo
+    void guardarConexiones(const std::string& nombreArchivo, Grafo<std::string>& grafo) const;
+    void escribirResultado(const std::string& nombre1, const std::string& nombre2, bool cumple,const std::string& nombreArchivo) const;
+    //En este se lee el archivo de personas para probar teoría de los 6 grados
+    void leerArchivo(const std::string& nombreArchivo, Grafo<std::string>& grafo);
 };
 
 #endif // MANEJADORARCHIVOS_H
